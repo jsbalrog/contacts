585c0226ba78fe46dbee323e84e442e2bb9288af
Ted Jenkins <jenkinset@jenkinset-Latitude-E6500.(none)>
2011-10-05 13:19:25 -0600

    Foundational steps

---
+++ js/views/Viewport.js
@@ -0,0 +1,23 @@
+Contacts.views.Viewport = Ext.extend(Ext.Panel, {
+	fullscreen: true,
+	layout : 'card',
+	cardSwitchAnimation: 'slide',
+	initComponent : function() {
+		// Put instances of cards into namespace
+		Ext.apply(Contacts, {
+//			contactList: new Contacts.views.ContactListPanel(),
+//			contactForm: new Contacts.views.ContactFormPanel()
+		});
+
+		// Put instances of cards (defined above) into viewport
+		Ext.apply(this, {
+			items: [
+//				Contacts.contactList,
+//				Contacts.contactForm
+			]
+		});
+
+		// Call superclass's initComponent method
+		Contacts.views.Viewport.superclass.initComponent.apply(this, arguments);
+	}
+});
